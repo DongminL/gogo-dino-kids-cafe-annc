2f91387d06bccb811927c6bb5775006731568826
DongminL <milkskfk12@gmail.com>
2026-04-11 23:07:37 +0900

    fix: playwright test report directory 이름 변경

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
+  outputDir: "./playwright-test-results",
   fullyParallel: false,
   retries: 0,
   reporter: "html",
