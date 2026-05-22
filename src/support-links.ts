1133c6538380001bd626ff12b6db13cdb1ae9d4b
DongminL <milkskfk12@gmail.com>
2026-05-22 15:38:34 +0900

    feat: github release tag 링크 추가

--- src/support-links.ts
+++ src/support-links.ts
@@ -1,2 +1,3 @@
 export const GITHUB_WIKI_URL = "https://github.com/DongminL/gogo-dino-kids-cafe-annc/wiki/Auto-Play-Time-Range-Settings-Guide";
 export const GOOGLE_FORM_URL = "https://forms.gle/BmBWJbFRmv31yTV38";
+export const GITHUB_RELEASE_TAG_URL = "https://github.com/DongminL/gogo-dino-kids-cafe-annc/releases/tag";
